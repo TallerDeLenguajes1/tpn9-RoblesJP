8f6066461fcd6d3ea50f375eb64c2d6017b9468f
RoblesJP <juan.robles1417@gmail.com>
2020-06-25 16:11:51 -0300

    Commit

---
+++ Morse/Helpers/ConversorDeMorse.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers
+{
+    public static class ConversorDeMorse
+    {
+        readonly static Dictionary<char, string> dic = new Dictionary<char, string>()
+        {
+            {'a', ".-"},
+            {'b', "-..."},
+            {'c', "-.-."},
+            {'d', "-.."},
+            {'e', "."},
+            {'f', "..-."},
+            {'g', "--."},
+            {'h', "...."},
+            {'i', ".."},
+            {'j', ".---"},
+            {'k', "-.-"},
+            {'l', ".-.."},
+            {'m', "--"},
+            {'n', "-."},
+            {'o', "---"},
+            {'p', ".--."},
+            {'q', "--.-"},
+            {'r', ".-."},
+            {'s', "..."},
+            {'t', "-"},
+            {'u', "..-"},
+            {'v', "...-"},
+            {'w', ".--"},
+            {'x', "-..-"},
+            {'y', "-.--"},
+            {'z', "--.."},
+            {' ', "/" }
+        };
+
+        public static string TextoAMorse(String texto)
+        {
+            texto = texto.ToLower();
+            string morse = "";
+
+            foreach(char caracter in texto.ToCharArray())
+            {
+                morse += dic[caracter] + " ";
+            }
+
+            return morse;
+        }
+
+        public static string MorseATexto(String morse)
+        {
+            string texto = "";
+
+            foreach (string codigo in morse.Split(" "))
+            {
+                foreach (KeyValuePair<char, string> res in dic)
+                {
+                    if (res.Value == codigo)
+                    {
+                        texto += res.Key;
+                    }
+                }
+            }
+            return texto;
+        }
+
+        
+    }
+}
---
+++ Morse/Main/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Helpers;
+using System.IO;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fecha = DateTime.Now.ToString("ddMMyyyy_hhmmss");
+
+            string texto;
+
+            string pathMorse = @"C:\Temp\morse_" + fecha + ".txt";
+            string pathTexto = @"C:\Temp\texto_" + fecha + ".txt";
+
+            Console.WriteLine(">> Ingrese un texto");
+            texto = Convert.ToString(Console.ReadLine());
+            File.WriteAllText(pathMorse, ConversorDeMorse.TextoAMorse(texto));
+            Console.WriteLine("-- El texto traducido a codigo morse se guardo en {0}", pathMorse);
+
+
+            string morse = File.ReadAllText(pathMorse);
+            File.WriteAllText(pathTexto, ConversorDeMorse.MorseATexto(morse));
+
+        }
+    }
+}
